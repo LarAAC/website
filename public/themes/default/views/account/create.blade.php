a32e0c5477d5d66b76794f6cdd9b3a726133dda4
Carlos Eduardo <juniob2ss@gmail.com>
2016-04-27 09:25:33 -0300

    Views account manager

---
+++ public/themes/default/views/account/create.blade.php
@@ -0,0 +1,48 @@
+<h2>Create Account</h2>
+
+<div class="content">
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <form action="#">
+      <div class="form-group form-group-sm">
+        <label for="">Account Name</label>
+        <input type="text" class="form-control" placeholder="4 to 30 characters">
+      </div>
+
+      <div class="form-group form-group-sm">
+        <label for="">Account Password</label>
+        <input type="text" class="form-control" placeholder="6 to 50 characters">
+        <p class="helper-block text-muted">
+          Do not use the same password that you use on other servers. It has come to our attention that owners of other servers will use their databases to hack accounts on My OTServ. Your password is securely stored with a one-way encryption in our database.
+        </p>
+      </div>
+
+      <div class="form-group form-group-sm">
+        <label for="">Repeat password</label>
+        <input type="text" class="form-control" placeholder="Repeat your password">
+      </div>
+
+      <div class="form-group form-group-sm">
+        <label for="">E-mail address</label>
+        <input type="text" class="form-control" placeholder="3 to 255 characters">
+        <p class="helper-block text-muted">Your e-mail is primarily used to recover your account, so please enter a legitimate e-mail or you won't be able to recover your account when you lose it.</p>
+      </div>
+
+      <div class="form-group form-group-sm">
+        <hr>
+        <p class="text-muted">By creating an account you agree to the <a href="#">My OTServ rules</a>. Violating the rules may result in a banishment or deletion of all your accounts.</p>
+      </div>
+
+      <div class="form-group form-group-sm">
+        <div class="col-md-6 alpha">
+          <input type="submit" class="btn btn-sm btn-block btn-primary">
+        </div>
+        <div class="col-md-6 omega">
+          <input type="reset" class="btn btn-sm btn-block btn-default">
+        </div>
+      </div>
+
+    </form>
+  </div>
+</div>
+</div>
---
+++ public/themes/default/views/account/login.blade.php
@@ -0,0 +1,32 @@
+<h2>Account Login</h2>
+
+<div class="content">
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <form action="#" class="form-horizontal">
+      <div class="form-group form-group-sm">
+        <label for="accountname">Account Name</label>
+        <input type="password" class="form-control">
+      </div>
+
+      <div class="form-group form-group-sm">
+        <label for="password">Password</label>
+        <input type="password" class="form-control">
+      </div>
+
+      <div class="form-group form-group-sm">
+        <div class="col-md-6 alpha">
+          <input type="submit" class="btn btn-sm btn-block btn-primary">
+        </div>
+        <div class="col-md-6 omega">
+          <input type="reset" class="btn btn-sm btn-block btn-default">
+        </div>
+      </div>
+
+      <div class="form-group form-group-sm text-right">
+        <a href="#">Forgot password?</a>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
